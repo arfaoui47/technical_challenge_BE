cb3cc5093dc724f419a3778c0392ebc8fc27c87a
Atef Arfaoui <arfaoui47@gmail.com>
2019-08-06 22:56:06 +0200

    add word creation

--- src/technical_challlenge_be/catalog_app/catalog_app/api.py
+++ src/technical_challlenge_be/catalog_app/catalog_app/api.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 from .views import BookViewSet
 from .views import CatalogueViewSet
+from .views import WordViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'Books'    , BookViewSet)
 router.register(r'Catalogue', CatalogueViewSet)
+router.register(r'Word'     , WordViewSet)
 urls = router.urls
--- src/technical_challlenge_be/catalog_app/catalog_app/views.py
+++ src/technical_challlenge_be/catalog_app/catalog_app/views.py
@@ -1,12 +1,27 @@
-from .models import Book, VocabularyCatalogue
+from .models import Book, VocabularyCatalogue, Word
 
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, status
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model  = Book
         fields = "__all__"
+
+    def create(self, validated_data):
+        self.save_worlds(validated_data)
+        return Book(**validated_data)
+
+    def save_worlds(self, data):
+        def filtered(word, filter):
+            return False
+
+        if 'content' in data:
+            content = data['content']
+            words   = content.split()
+            for word in words:
+                if not filtered(word, filter):
+                    Word.objects.create(value=word, vocabulary=data['vocabulary'])
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -23,3 +38,14 @@ class CatalogueSerializer(serializers.HyperlinkedModelSerializer):
 class CatalogueViewSet(viewsets.ModelViewSet):
     queryset         = VocabularyCatalogue.objects.all()
     serializer_class = CatalogueSerializer
+
+
+class WordSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model  = Word
+        fields = "__all__"
+
+
+class WordViewSet(viewsets.ModelViewSet):
+    queryset         = Word.objects.all()
+    serializer_class = WordSerializer
